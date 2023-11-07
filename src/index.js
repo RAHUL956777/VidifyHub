1740899b5a2e98caf88d1ab44f7a230bc2c773ea
RAHUL <dasr16583@gmail.com>
2023-11-07 08:54:39 +0530

    adding more effieient code for handling error

--- src/index.js
+++ src/index.js
@@ -9,21 +9,15 @@ connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running at port ${process.env.PORT}`);
+      app.on("error", (error) => {
+        console.log(error);
+        throw error;
+      });
     });
   })
   .catch((error) => {
     console.log("Mongodb connection failed !!!", error);
   });
-
-
-
-
-
-
-
-
-
-
 
 /*
 
