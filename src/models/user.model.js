6491350e3b63399ae79597b5cecac236c2b526e6
RAHUL <dasr16583@gmail.com>
2023-11-13 21:25:30 +0530

    working on jwt models

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new Schema(
       lowercase: true,
       trim: true,
     },
-    fullname: {
+    fullName: {
       type: String,
       required: true,
       trim: true,
@@ -57,6 +57,30 @@ userSchema.pre("save",async function(next){
 
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id: this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRETE ,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY,
+    }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id: this._id,
+    },
+    process.env.AREFRESH_TOKEN_SCERETE ,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY,
+    }
+    )
 }
 
 export const User = mongoose.model("User", userSchema);
